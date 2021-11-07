d7727fc489ec0e103b9b74c37d51616113e7fe2c
Lindsey Stearns <list4199@gmail.com>
2021-11-06 18:02:25 -0600

    Final Challenge Push

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -35,7 +35,26 @@ ORDER BY _____, _____ DESC;
 -- Retrieve the number of employees by their most recent job title who are about to retire.
 SELECT COUNT(ut.emp_no), 
 ut.title
--- INTO retiring_titles
+INTO retiring_titles
 FROM unique_titles as ut 
 GROUP BY ut.title
 ORDER BY ut.count DESC; 
+
+-- Mentorship Eligibility Table
+SELECT DISTINCT ON (e.emp_no)
+e.emp_no,
+    e.first_name,
+e.last_name,
+e.birth_date,
+    de.from_date
+de.to_date,
+    ti.title,
+INTO mentorship_eligibility
+FROM employees as e 
+INNER JOIN dept_emp as de 
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no, ti.from_date DESC;
